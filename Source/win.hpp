d1bd62c6928e785a089da3f2cb53e30bdf449d31
circonfl3x <74814824+circonfl3x@users.noreply.github.com>
2021-11-15 20:03:26 +0300

    `WinCase()` is now much more concise
    instead of having a different `else-if` block for each case, they're now checked in one if statement using the `&&` and `||` operators

--- Source/win.hpp
+++ Source/win.hpp
@@ -6,36 +6,14 @@ bool checkTrue(list <int> lr, int tolook)
 bool WinCase(list <int> lr)
 {
 
-	if(checkTrue(lr, 1) && checkTrue(lr, 2) && checkTrue(lr, 3))
-	{
-		return true;
-	}
-	else if(checkTrue(lr, 4) && checkTrue(lr, 5) && checkTrue(lr, 6))
-	{
-		return true;
-	}
-	else if(checkTrue(lr, 7) && checkTrue(lr, 8) && checkTrue(lr, 9))
-	{
-		return true;
-	}
-	else if(checkTrue(lr, 1) && checkTrue(lr, 4) && checkTrue(lr, 7))
-	{
-		return true;
-	}
-	else if(checkTrue(lr, 2) && checkTrue(lr, 5) && checkTrue(lr, 8))
-	{
-		return true;
-	}
-	else if(checkTrue(lr, 3) && checkTrue(lr, 6) && checkTrue(lr, 9))
-	{
-		return true;
-	}
-	else if(checkTrue(lr, 1) && checkTrue(lr, 5) && checkTrue(lr, 9))
-	{
-		return true;
-	}
-	else if(checkTrue(lr, 3) && checkTrue(lr, 5) && checkTrue(lr, 7))
-	{
+	if(checkTrue(lr, 1) && checkTrue(lr, 2) && checkTrue(lr, 3)||
+	checkTrue(lr, 4) && checkTrue(lr, 5) && checkTrue(lr, 6) ||
+	checkTrue(lr, 7) && checkTrue(lr, 8) && checkTrue(lr, 9)||
+	checkTrue(lr, 1) && checkTrue(lr, 4) && checkTrue(lr, 7) ||
+	checkTrue(lr, 2) && checkTrue(lr, 5) && checkTrue(lr, 8) ||
+	checkTrue(lr, 3) && checkTrue(lr, 6) && checkTrue(lr, 9) ||
+	checkTrue(lr, 1) && checkTrue(lr, 5) && checkTrue(lr, 9) || 
+	checkTrue(lr, 3) && checkTrue(lr, 5) && checkTrue(lr, 7)){
 		return true;
 	}
 
