ce683c68bdd3aabf2b3dd2f9b8f1a315a670def7
circonfl3x <74814824+circonfl3x@users.noreply.github.com>
2021-11-15 20:10:58 +0300

    Deprecated as available in main file

--- Source/posavail.hpp
+++
@@ -1,7 +0,0 @@
-
-bool ispa(list <int> arr, list <int> arr2, int a)
-{
-    if(find(arr.begin(), arr.end(), a) != arr.end() ) return true;
-    else if (find(arr2.begin(), arr2.end(), a) != arr2.end()) return true;
-    else return false;
-}
